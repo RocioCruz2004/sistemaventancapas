396183ee43be231336226ba1c0fc6d10e7995a01
Rocío <tj.rocio.cruz.z@upds.net.bo>
2024-03-13 20:06:15 -0400

    v2 base de datos, clases tablas

---
+++ SistemaVentas/SistemasVentas.Modelos/Provee.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemasVentas.Modelos
+{
+    public class Provee
+    {
+        public int IdProvee { get; set; }
+        public int IdProducto { get; set; }
+        public int IdProveedor { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
